3810d175dea7d3d0cd34d606ee88cb25ece884d0
schlöpp! <67605203+schlopp@users.noreply.github.com>
2021-05-12 09:26:12 +0200

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -12,6 +12,11 @@ class MembersCog(commands.Cog):
     async def ping(self, ctx:commands.Context):
         return await ctx.send(f'🏓 pong! Latency: `{round(self.bot.latency, 8)}ms`')
     
+    @commands.command(aliases=["latency"])
+    @commands.bot_has_permissions(send_messages=True)
+    async def ping(self, ctx:commands.Context):
+        return await ctx.send('https://github.com/MysticVanguard/AquaMarine')
+    
     @commands.command(aliases=["say"])
     @commands.bot_has_permissions(send_messages=True)
     async def echo(self, ctx:commands.Context, *, content:str):
