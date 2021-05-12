a5ae280931556b279ec5fc905c6006ea7c78d7d3
schlöpp! <67605203+schlopp@users.noreply.github.com>
2021-05-12 23:07:12 +0200

    Update fishing_handler.py

--- utils/fishing_handler.py
+++ utils/fishing_handler.py
@@ -1,1 +1,21 @@
+from os import walk
 
+def fetch_fish(dir:str) -> dict:
+    """Fetch fish given a directory."""
+    fetched_fish = {
+        "common": [], 
+        "uncommon": [], 
+        "rare": [],
+        "epic": [],
+        "legendary": [],
+        "mythic": [],
+    }
+    
+    _, _, filenames = next(walk(dir))
+    for i in filenames:
+        splitted = i.split("_")
+        try:
+            fetched_fish[splitted[0]].append(" ".join(splitted[1:]).title())
+        except KeyError:
+            pass
+    return fetched_fish
