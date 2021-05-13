94f1401a9e435ec97a82ff0d41ee67404d4133d2
schlöpp! <67605203+schlopp@users.noreply.github.com>
2021-05-13 23:15:26 +0200

    Added balance command

--- cogs/fishing.py
+++ cogs/fishing.py
@@ -8,10 +8,16 @@ class MembersCog(commands.Cog):
         self.bot = bot
     
     
-    @commands.command()
+    @commands.command(aliases=["bal"])
     @commands.bot_has_permissions(send_messages=True)
-    async def fish(self, ctx:commands.Context):
-        pass
+    async def balance(self, ctx:commands.Context):
+        async with utils.DatabaseConnection() as db:
+            fetched = await db("""SELECT * FROM user_balance WHERE user_id = $1""", ctx.author.id)
+        
+        if fetched:
+            await ctx.send(f"You have {feteched[0]['balance']} money!")
+        else:
+            await ctx.send("You have no money!")
 
 def setup(bot):
     bot.add_cog(MembersCog(bot))
